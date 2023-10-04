face86701e42fecdbad24c11ac1f7ddc7060a2f9
Tom Hong <tom.y.y.hong@gmail.com>
2023-10-04 10:48:39 -0400

    add the component folder

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+           <code>code part</code> 
         </p>
       </div>
     </>
